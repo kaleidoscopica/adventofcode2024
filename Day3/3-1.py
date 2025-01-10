5e43609f949891ac2fde65c95b118b31e0f72954
kaleidoscopica <rayearth53@yahoo.com>
2025-01-10 09:50:01 -0600

    Day 3-2 complete

--- Day3/3-1.py
+++ Day3/3-1.py
@@ -5,15 +5,15 @@ def main():
   multiplicands = []
 
   with open('input.txt') as file:
-    data = file.readlines()
+    # Join all the lines together, into a single line
+    data = " ".join(line.strip() for line in file)
   file.close
 
   # Use regex to find all strings that look like 'mul(123,123)', and add them to a list
-  for line in data:
-    mul = re.findall('mul\([0-9]+\,[0-9]+\)', line)
-    # Then parse each one to grab its pair of numbers that will be multiplied
-    for item in mul:
-      multiplicands.append(re.findall('[0-9]+', item))
+  mul = re.findall('mul\([0-9]+\,[0-9]+\)', data)
+  # Then parse each one to grab its pair of numbers that will be multiplied
+  for item in mul:
+    multiplicands.append(re.findall('[0-9]+', item))
 
   # Multiply each pair together and add it to a total
   total = 0
--- Day3/3-2.py
+++ Day3/3-2.py
@@ -5,31 +5,29 @@ def main():
   multiplicands = []
 
   with open('input.txt') as file:
-    data = file.readlines()
+    # Join all the lines together, into a single line
+    data = " ".join(line.strip() for line in file)
   file.close
 
   # First insert a 'do()' at the beginning since mul() instructions are enabled at start
-  #print("data[0] is: ", data[0])
-  data[0] = 'do()' + data[0]
-  #print("data[0] is now: ", data[0])
+  data = 'do()' + data
 
   # Use regex to find all strings that look like 'mul(123,123)', and add them to a list
-  for line in data:
-    # Split each line where you see a "don't()"
-    donts = re.split('don\'t\(\)', line)
-    print("Donts for this line are.......")
-    print()
-    print()
-    print(donts)
-    # Then find the index where you start to see a 'do()'
-    for dont in donts:
-      do = dont.index('do()') if 'do()' in dont else -1
-      # Only grab the multiplicands after the 'do()', if it exists
-      if do != -1:
-        mul = re.findall('mul\([0-9]+\,[0-9]+\)', line[do:])
-        # Then parse each one to grab its pair of numbers that will be multiplied
-        for item in mul:
-          multiplicands.append(re.findall('[0-9]+', item))
+  # Split everywhere you see a "do()"
+  dos = re.split('do\(\)', data)
+
+  # Then find the index where you start to see a 'don't()'
+  for do in dos:
+    dont = do.index("don't()") if "don't()" in do else -1
+    # Only grab the multiplicands before the 'don't()', if it exists
+    if dont == -1:
+      mul = re.findall('mul\([0-9]+\,[0-9]+\)', do)
+    else:
+      mul = re.findall('mul\([0-9]+\,[0-9]+\)', do[:dont])
+    # Then parse each one to grab its pair of numbers that will be multiplied
+    for item in mul:
+      multiplicands.append(re.findall('[0-9]+', item))
+
 
   # Multiply each pair together and add it to a total
   total = 0
