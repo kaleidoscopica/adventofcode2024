f89c45b7dfc6ab954d2d7a278f32219ace5fdae1
kaleidoscopica <rayearth53@yahoo.com>
2025-01-10 21:03:05 -0600

    Day 5-1 complete

--- Day5/5-1.py
+++ Day5/5-1.py
@@ -15,20 +15,28 @@ def main():
   total = 0
 
   for update in updates:
-    valid = False
-    page_rules = []
-    for number in update:
-      # Check where the rules containing that page number are
-      for rule in rules:
-        print(rule)
-        if number in rule:
-          page_rules.append(rule)
-      print("For number...", number)
-      #print("The applicable rules are:", page_rules)
+    # Assume each update is valid
+    valid = True
+    for idx, number in enumerate(update):
+      # As long as we're not already at the last number, grab the next number to check
+      if idx < len(update)-1:
+        next_number = update[idx+1]
+        print("Comparing {0} and {1}...".format(number, next_number))
+
+        # Go through the rules, and find the one that applies
+        for rule in rules:
+          if number in rule and next_number in rule:
+            print("Evaluating rule", rule)
+            # If the rule has the numbers out-of-order, the update is not correctly-ordered
+            if rule[0] == next_number and rule[1] == number:
+              print("These numbers are out of order.")
+              valid = False
+            else:
+              print("These numbers are in the correct order!")
   
     # If the update is correctly-ordered, add its middle page number to the total
     if valid == True:
-      total += update[len(update)//2]
+      total += int(update[len(update)//2])
 
   print("The total is:", total)
 
