aa0de3c4068cc65520e4f08a3ef47f5187b503b0
kaleidoscopica <rayearth53@yahoo.com>
2025-01-12 10:30:02 -0600

    Day 5-2 complete

--- Day5/5-2.py
+++ Day5/5-2.py
@@ -11,36 +11,57 @@ def main():
   file.close()
 
   total = 0
+  out_of_order = []
 
   for update in updates:
     # Assume each update is valid
     valid = True
-    print("Before any changes.....", update)
 
     for idx, number in enumerate(update):
       # As long as we're not already at the last number, grab the next number to check
       if idx < len(update)-1:
         next_number = update[idx+1]
-        print("Comparing {0} and {1}...".format(number, next_number))
-
         # Go through the rules, and find the one that applies
         for rule in rules:
           if number in rule and next_number in rule:
-            print("Evaluating rule", rule)
             # If the rule has the numbers out-of-order, the update is not correctly-ordered
             if rule[0] == next_number and rule[1] == number:
-              print("These numbers are out of order. Let's switch them around.")
               valid = False
-              # Switch them around to put them in the right order
-              update[idx], update[idx+1] = update[idx+1], update[idx]
-            else:
-              print("These numbers are already in the correct order!")
 
-    print("After changes...", update)
-    # If the update was incorrectly-ordered, add its middle page number to the total
+    # Keep only the out-of-order updates in a separate list for further processing
     if valid == False:
-      print("Adding middle number to total:", update[len(update)//2])
-      total += int(update[len(update)//2])
+      out_of_order.append(update)
+
+  # Now loop through the out-of-order updates and get them in order
+  for update in out_of_order:
+    # We know each update is not yet valid, at least at the start
+    valid = False
+
+    # While it's not yet valid, keep making changes
+    while valid == False:
+      changes = 0
+      for idx, number in enumerate(update):
+      # As long as we're not already at the last number, grab the next number to check
+        if idx < len(update)-1:
+          next_number = update[idx+1]
+          # Go through the rules, and find the one that applies
+          for rule in rules:
+            if number in rule and next_number in rule:
+              # If the rule has the numbers out-of-order, the update is not correctly-ordered
+              if rule[0] == next_number and rule[1] == number:
+                # Switch them around to put them in the right order
+                update[idx], update[idx+1] = update[idx+1], update[idx]
+                changes += 1
+      
+      # If no changes were made, it can become valid, stopping the while loop
+      if changes != 0:
+        valid = False
+      else:
+        valid = True
+
+  # For each originally out-of-order update, add its middle page number to the total
+  for update in out_of_order:
+    total += int(update[len(update)//2])
 
   print("The total is:", total)
 
