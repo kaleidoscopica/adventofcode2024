d93b7a7d6e038eaa8843ea6ca5dc8baff0a0c88a
kaleidoscopica <rayearth53@yahoo.com>
2025-01-13 15:54:20 -0600

    Working on Day 7-2

--- Day7/7-2.py
+++ Day7/7-2.py
@@ -14,10 +14,14 @@ def main():
   total = 0
 
   for idx, equation in enumerate(equations):
+    #print("Evaluating numbers...", equation)
+    #print("Test value here is", values[idx])
     found = False
     # For each equation, generate a list of all possible combinations of + and * operators to try
     num_operators = len(equation)-1
-    ops_list = [list(x) for x in product('+*', repeat=num_operators)]
+    ops_list = [list(x) for x in product('+*|', repeat=num_operators)]
+    #print(ops_list)
+
     # Then try each list of operators zipped against the equation, and see if evaluating it matches the given value
     for operators in ops_list:
       if found == False:
@@ -25,18 +29,30 @@ def main():
         eq = [x for y in zip_longest(equation, operators) for x in y]
         # Since the shorter list left a NoneType at the end when zipping, remove it
         del eq[-1]
+        # Whenever you see a '|', just remove it from the list
+        while '|' in eq:
+            eq.remove('|')
         # Add closing parentheses after every digit except the first one
+        paren_count = 0
         for index, i in enumerate(eq):
           if i.isdigit() and index > 1:
-            eq.insert(index+1, ')')
+            if index < len(eq)-1:
+              if eq[index+1].isdigit():
+                pass
+            else:
+              eq.insert(index+1, ')')
+              paren_count += 1
         # Add that amount of opening parentheses at the beginning (len(equation)-1)
-        for i in range(len(equation)-1):
+        for i in range(paren_count):
           eq.insert(0, '(')
         # Now join all the items in the list into one big concatenated equation and evaluate it
         evaluation = eval(''.join(eq))
+        #print("Evaluation:", evaluation)
         if evaluation == values[idx]:
+          #print("{0} is possible. Values being added to total: {1}".format(''.join(eq), values[idx]))
           found = True
           total += values[idx]
+          #print("Running total:", total)
 
   print("The total value of all possible true equations is:", total)
 
