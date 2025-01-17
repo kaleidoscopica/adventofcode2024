a4e9f3f49341acb5e89160d42cfb314e422f2936
kaleidoscopica <rayearth53@yahoo.com>
2025-01-16 21:41:13 -0600

    Starting 8-2

--- Day8/8-1.py
+++ Day8/8-1.py
@@ -5,17 +5,21 @@ def main():
     map = file.read().splitlines()
   file.close()
 
-  print(len(map[0]))
+  antinodes = []
+  antennas_checked = []
 
   for idx1, row in enumerate(map):
     for idx2, col in enumerate(row):
       pos = encode(idx1, idx2)
       if col != '.':
-        for i in range(2500-pos):
-          
+        # If we haven't checked this type of antenna yet, find all instances of it
+        if col not in antennas_checked:
+          antennas_checked.append(col)
+          for i in range(2499-pos):
+            pass
 
 def encode(row, col):
-  return row*50 + col + 1
+  return row*50 + col
 
 def decode(n):
   row = n // 50 
