9b885338efbb6f22fed479a7ceb39b32b7fa3487
kaleidoscopica <rayearth53@yahoo.com>
2025-01-16 21:41:06 -0600

    Starting 8-2

--- Day8/8-1.py
+++ Day8/8-1.py
@@ -5,6 +5,21 @@ def main():
     map = file.read().splitlines()
   file.close()
 
-  print(map)
+  print(len(map[0]))
+
+  for idx1, row in enumerate(map):
+    for idx2, col in enumerate(row):
+      pos = encode(idx1, idx2)
+      if col != '.':
+        for i in range(2500-pos):
+          
+
+def encode(row, col):
+  return row*50 + col + 1
+
+def decode(n):
+  row = n // 50 
+  col = n % 50
+  return row, col
 
 main()
