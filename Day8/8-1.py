b060434e2a2d31149524b7e1c93dd9cd54562195
kaleidoscopica <rayearth53@yahoo.com>
2025-01-31 12:50:30 -0600

    Latest testing

--- Day8/8-1.py
+++ Day8/8-1.py
@@ -12,11 +12,14 @@ def main():
     for idx2, col in enumerate(row):
       pos = encode(idx1, idx2)
       if col != '.':
+        print("Evaluating antenna", col)
         # If we haven't checked this type of antenna yet, find all instances of it
         if col not in antennas_checked:
           antennas_checked.append(col)
           for i in range(2499-pos):
-            pass
+            r, c = decode(i)
+            if map[r][c] == col:
+              print("Founding matching antenna at position", i)
 
 def encode(row, col):
   return row*50 + col
